8775954eb37ebc8a1a667792964c9a48b99cc712
Leon Tur <tutrkov.leon-2@yandex.ru>
2023-02-03 19:48:26 +0300

    Added class

--- Person.java
+++ Person.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 public class Person {
     public String name;
     public Person(String input_name){
         this. name = input_name;
     }
-    
+    ArrayList <Person> listName = new ArrayList<>();
+    public void AddName(Person p){
+        listName.add(p);
+    }
+    public ArrayList<Person> GetListName(){
+        return listName;
+    }
 }
